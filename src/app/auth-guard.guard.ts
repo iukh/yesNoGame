13e86c86957cdcecd743a2ac98d6d13b14e121c2
iukh <33911363+iukh@users.noreply.github.com>
2019-02-18 06:59:39 +0300

    Update auth-guard.guard.ts

--- src/app/auth-guard.guard.ts
+++ src/app/auth-guard.guard.ts
@@ -1,12 +1,19 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
-import { Observable } from 'rxjs';
+import { CookieService } from 'ngx-cookie-service';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuardGuard implements CanActivate {
+  constructor(private cookieService: CookieService, private router: Router) { }
   canActivate(): boolean {
-    return true;
+    if (this.cookieService.get('userId')) {
+      return true;
+    } else {
+      this.router.navigate(['/auth']);
+      return false;
+    }
   }
 }
