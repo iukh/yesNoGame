4cd2706901c1fc1eec25477333d72e7d6099fb6d
iukh <33911363+iukh@users.noreply.github.com>
2019-02-18 06:53:44 +0300

    Create auth-guard.guard.ts

---
+++ src/app/auth-guard.guard.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuardGuard implements CanActivate {
+  canActivate(): boolean {
+    return true;
+  }
+}
