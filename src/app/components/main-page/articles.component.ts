b6a9c24c955caabd9204a9a03d9fe50abd8da710
iukh <33911363+iukh@users.noreply.github.com>
2019-02-19 02:01:10 +0300

    Update articles.component.ts

--- src/app/components/main-page/articles.component.ts
+++ src/app/components/main-page/articles.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Article } from '../../models/article';
 import { ArticleService } from '../../services/article.service';
+import { Section } from '../../models/section';
+import { SectionService } from '../../services/section.service';
 @Component({
   selector: 'app-articles',
   templateUrl: './articles.component.html',
@@ -8,13 +10,22 @@ import { ArticleService } from '../../services/article.service';
 })
 export class ArticlesComponent implements OnInit {
   articles: Article[] = [];
-  constructor(private articleService: ArticleService) { }
+  sections: Section[];
+  constructor(private articleService: ArticleService,
+              private sectionService: SectionService) { }
   ngOnInit() {
     this.getArticles();
+    this.getSections();
   }
   getArticles() {
     return this.articleService.getArticles().subscribe(articles => {
       this.articles = articles;
+    });
+  }
+  getSections() {
+    return this.sectionService.getSections().subscribe(sections => {
+      this.sections = sections;
+      console.log(this.sections);
     });
   }
 }
