f65af6449071574f33450ed77bbe35ab2f8dbf60
iukh <33911363+iukh@users.noreply.github.com>
2019-02-24 21:32:06 +0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test-project';
+  userRole = false;
+  changeUserStatus(value) {
+    this.userRole = value;
+    console.log('event here' + value);
+  }
 }
